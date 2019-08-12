4174ea4475902d5f2d35aa0737e1961124223547
john <johnwee35@gmail.com>
2019-08-12 16:49:45 +0800

    add navbar and navbar.test

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from "react-bootstrap/Navbar";
+
+function NarBar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NarBar
