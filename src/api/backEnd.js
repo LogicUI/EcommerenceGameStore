fd930b0b898eb77974d149a9cb3be86a23639601
john <johnwee35@gmail.com>
2019-08-12 16:49:05 +0800

    deleted old components

--- src/api/backEnd.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-
-export default axios.create({
-    baseURL: "https://ecommerence-backend.herokuapp.com"
-})
--- src/jss/HomeStyles.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-const HomeStyles = makeStyles({
-  root: {
-    overflow: 'hidden'
-  }
-});
-
-export default HomeStyles;
