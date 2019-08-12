df28bc3141c80e91620e52c0365aecbbe715bcb0
john <johnwee35@gmail.com>
2019-08-12 16:49:36 +0800

    add app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,6 @@
-import React, { useContext } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Header from './Header';
-import Hero from './Hero';
-import Home from './Home';
-import Games from './Games';
-import Consoles from './Consoles';
-import Videos from './Videos';
-import { Store } from '../global/Tabs';
-
+import React from 'react';
 const App = () => {
-  const hook = useContext(Store);
-  return (
-    <section>
-      <Hero />
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={() => {
-              hook.handleLoad(0);
-              return <Home />;
-            }}
-          />
-          <Route
-            path="/games"
-            render={() => {
-              hook.handleLoad(1);
-
-              return <Games />;
-            }}
-          />
-          <Route
-            path="/videos"
-            render={() => {
-              hook.handleLoad(2);
-
-              return <Videos />;
-            }}
-          />
-          <Route
-            path="/consoles"
-            render={() => {
-              hook.handleLoad(3);
-
-              return <Consoles />;
-            }}
-          />
-        </Switch>
-      </BrowserRouter>
-    </section>
-  );
+  return <div>rwar</div>;
 };
 
 export default App;
