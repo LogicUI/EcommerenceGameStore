b21d5ec1067f011f1670af0e299f4b1aa82db400
john <johnwee35@gmail.com>
2019-08-12 16:49:30 +0800

    add yarn dependencies

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import TabsProvider from './global/Tabs';
-import CartProvider from './global/Cart';
-import CheckoutProvider from './global/Checkout';
-import AuthProvider from './global/Authen';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <AuthProvider>
-    <CheckoutProvider>
-      <CartProvider>
-        <TabsProvider>
-          <App />
-        </TabsProvider>
-      </CartProvider>
-    </CheckoutProvider>
-  </AuthProvider>,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
