ed69823ed90614c99e6f74fc937dab827b120b98
john <johnwee35@gmail.com>
2019-08-15 16:02:39 +0800

    update app component to add home component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import HeaderNav from './HeaderNav/HeaderNav';
+import HeaderNav from '../HeaderNav/HeaderNav';
+import Home from '../Home/Home';
 const App = () => {
   return (
     <div>
       <HeaderNav />
+      <Home />
     </div>
   );
 };
