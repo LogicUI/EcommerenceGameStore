bf8e3461635ff6e8a88264e30ab092e08ddc0027
john <johnwee35@gmail.com>
2019-08-17 23:55:45 +0800

    add app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HeaderNav from '../HeaderNav/HeaderNav';
 import Home from '../Home/Home';
+import axios from '../../api/axios';
+
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get('/products');
+      setProducts(response.data);
+    })();
+  },[]);
+
   return (
     <div>
       <HeaderNav />
-      <Home />
+      <Home products={products}/>
     </div>
   );
 };
