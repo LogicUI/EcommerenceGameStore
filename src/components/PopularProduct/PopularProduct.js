cbe06ca5531e02c4e5fb4a1a71593d392f0da2ee
john <johnwee35@gmail.com>
2019-08-19 22:40:02 +0800

    add release date for prerelease item

--- src/components/PopularProduct/PopularProduct.js
+++ src/components/PopularProduct/PopularProduct.js
@@ -3,15 +3,16 @@ import { Card, Button } from 'react-bootstrap';
 import { CardProduct, Title, SubTitle } from './PopularProduct.styles.js';
 import Ratings from '../Ratings/Ratings';
 
-const Product = ({ image, title, price, rating }) => {
+const Product = ({ image, title, price, rating, release }) => {
   return (
     <CardProduct>
       <Card.Img variant="top" src={image} width="200px" height="200px" />
-      <Card.Body>
+      <Card.Body style={{ marginBottom: '2rem' }}>
         <Title>{title}</Title>
         <SubTitle className="text-danger">${price}</SubTitle>
         <Card.Text>
           <Ratings rating={rating} />
+          {release && <h4 className="text-center">Release Date:{release}</h4>}
         </Card.Text>
 
         <Button variant="primary">Add To Cart</Button>
