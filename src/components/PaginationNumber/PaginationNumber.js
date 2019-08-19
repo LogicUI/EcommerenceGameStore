2f8aa2c362d2bfcf5d3e443d50fffe50bbafa9a9
john <johnwee35@gmail.com>
2019-08-19 22:13:28 +0800

    fix paginationNumber

--- src/components/PaginationNumber/PaginationNumber.js
+++ src/components/PaginationNumber/PaginationNumber.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Pagination } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 
 const PaginationNumber = ({ itemsPerPage, totalItems, paginate }) => {
   const pageNumbers = [];
-  for (let index = 1; index < Math.ceil(totalItems / itemsPerPage); index++) {
+
+  for (let index = 1; index <= Math.ceil(totalItems / itemsPerPage); index++) {
     pageNumbers.push(index);
   }
-
   return (
     <Pagination>
       {pageNumbers.map((page) => {
